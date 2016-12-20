efbe9e402c1348b0151844d4875e3d8c7145cd8b
Nerijus Abromavicius <nerijus.abromavicius@adform.com>
2016-12-20 18:49:53 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -8,26 +8,18 @@ function byClass (id){
 }
 
 
-// windnow.onscroll = function(){
-
-// 	console.log("asd");
-// 	}
-
-
-
+// 
 
 byId("kode").onclick = function(){
-
-	document.getElementsByClassName("content2")[0].style.webkitTransform = 'translate(0px, -100%)';
-	document.getElementsByClassName("content")[0].style.webkitTransform = 'translate(0px, -100%)';
-
+	byClass("content2")[0].style.webkitTransform = 'translate(0px, -100%)';
+	byClass("content")[0].style.webkitTransform = 'translate(0px, -100%)';
 
 	}
 byId("name").onclick = function(){
-	document.getElementsByClassName("content2")[0].style.webkitTransform = 'translate(0px, 0px)';
-	document.getElementsByClassName("content")[0].style.webkitTransform = 'translate(0px, 0px)';
-	byId("kode").style.opacity = "1";
+	byClass("content2")[0].style.webkitTransform = 'translate(0px, 0px)';
+	byClass("content")[0].style.webkitTransform = 'translate(0px, 0px)';
 	}
+
 byId("kode").onmouseover = function(){
 	byId("kode").style.opacity = "0.8";
 	}
@@ -35,20 +27,24 @@ byId("kode").onmouseleave = function(){
 	byId("kode").style.opacity = "1";
 	}
 
-// function clikedOne(){
-// 	document.getElementsByClassName("content2")[0].style.WebkitTransition = "all 20s"
-// 	byId("back").style.webkitTransform = "rotate(0deg)";
-// 	document.getElementsByClassName("content2")[0].style.webkitTransform = 'translate(0px, -100%)';
-// 	byClass("content3")[0].scrollIntoView();
-// 	byId("icons").style.display = "inline";
-// }	
-// function clikedTwo(){
-// 	byId("back").style.webkitTransform = "rotate(180deg)";
-// 	byClass("content3")[0].style.height = "0px";
-// 	byId("icons").style.display = "none";
-// }	
+function clikedOne(){
+	byId("back").style.webkitTransform = "rotate(0deg)";
+	byClass("content2")[0].style.webkitTransform = 'translate(0px, -106%)';
+	byId("icons").style.display = "inline-flex";
+}	
+function clikedTwo(){
+	// byId("back").style.webkitTransform = "rotate(180deg)";
+	// byClass("content3")[0].style.height = "0px";
+	// byId("icons").style.display = "none";
+}	
 
-
+byId("back").onclick = function(){
+	if(window.getComputedStyle(byClass("content3")[0], null).getPropertyValue("height") == "0px"){
+		clikedOne();
+	}else{
+		clikedTwo();
+	}
+}
 
 setTimeout(function(){
 	document.getElementById("embed_npfido").style.display = "none";
