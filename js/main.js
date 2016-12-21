679e48e9474ac8ec48e35fb53cfe6b35ae97f71e
Nerijus Abromavicius <nerijus.abromavicius@adform.com>
2016-12-21 17:42:15 +0200

    css2

--- js/main.js
+++ js/main.js
@@ -13,35 +13,43 @@ function byClass (id){
 byId("kode").onclick = function(){
 	byClass("content2")[0].style.webkitTransform = 'translate(0px, -100%)';
 	byClass("content")[0].style.webkitTransform = 'translate(0px, -100%)';
+	byId("back").style.display = "block";
+}
 
-	}
 byId("name").onclick = function(){
 	byClass("content2")[0].style.webkitTransform = 'translate(0px, 0px)';
 	byClass("content")[0].style.webkitTransform = 'translate(0px, 0px)';
-	}
+	byId("icons").style.display = "none";
+}
 
 byId("kode").onmouseover = function(){
 	byId("kode").style.opacity = "0.8";
-	}
+}
+
 byId("kode").onmouseleave = function(){
 	byId("kode").style.opacity = "1";
-	}
+}
 
 function clikedOne(){
 	byId("back").style.webkitTransform = "rotate(0deg)";
-	byClass("content2")[0].style.webkitTransform = 'translate(0px, -106%)';
 	byId("icons").style.display = "inline-flex";
+	byClass("content2")[0].style.webkitTransform = 'translate(0px, -107%)';
+	console.log(byClass("content2")[0].offsetTop);	
 }	
+
 function clikedTwo(){
-	// byId("back").style.webkitTransform = "rotate(180deg)";
-	// byClass("content3")[0].style.height = "0px";
-	// byId("icons").style.display = "none";
+	byId("back").style.webkitTransform = "rotate(180deg)";
+	byClass("content2")[0].style.webkitTransform = 'translate(0px, -100%)';
+	setTimeout(function(){
+		byId("icons").style.display = "none";
+	}, 1000)
 }	
 
 byId("back").onclick = function(){
-	if(window.getComputedStyle(byClass("content3")[0], null).getPropertyValue("height") == "0px"){
+	if(byId("icons").offsetTop == 0 ){
 		clikedOne();
-	}else{
+	}
+	else {
 		clikedTwo();
 	}
 }
